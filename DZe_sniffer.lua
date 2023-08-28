50c338a2268655d29a4d20c69a5b2cbe17ffa6e7
ZGXaimware <140361278+ZGXaimware@users.noreply.github.com>
2023-08-28 09:41:51 +0800

    modified:   DZe_sniffer.lua

--- DZe_sniffer.lua
+++ DZe_sniffer.lua
@@ -1,27 +1,4 @@
--- Aimware-DangerZone-Lua
---Last Updated 2023/8/15 1.1.5 (New Version)
-
--- local tab = gui.Tab(gui.Reference("Misc"), "DZesniffer", "DangerZone Elite Sniffer");
--- local main_box = gui.Groupbox(tab, "Sniffer", 16, 16, 400, 0);
--- local ranks_mode = gui.Combobox(main_box, "tablet.mode", "Message SendWay",
---     "Only Console", "In Party chat", "In Team Chat")
--- local messagemaster = gui.Checkbox(main_box, "tablet.master", "Master Switch", 1)
--- -- local purchasemaster = gui.Checkbox(main_box, "tablet.purchasemaster", "Purchase sniffer", 1)
--- local respawnmaster = gui.Checkbox(main_box, "tablet.respawnmaster", "Respawn sniffer", 1)
--- local exitmaster = gui.Checkbox(main_box, "tablet.exitmaster", "Exit sniffer", 1)
--- local paradropmaster = gui.Checkbox(main_box, "tablet.paradropmaster", "ParaDrop sniffer", 1)
--- local dronedispatchmaster = gui.Checkbox(main_box, "tablet.dronedispatchmaster", "Purchase Sniffer", 0)
--- local gotvswitch = gui.Combobox(main_box, "tablet.gotvswitch", "GOTV Selection", "Off", "Disable on GOTV",
---     "Force Enable on GOTV");
-
-
-
-
 local panorama = require("panorama library.lua")
-
-
-
-
 local ui = {}
 ui.group_name = "DangerZone Elite Sniffer"
 ui.messagemode = menu.add_selection(ui.group_name, "SendWay", { "Only Console", "Partychat", "Teamchat" })
@@ -31,17 +8,6 @@ ui.exitmaster = menu.add_checkbox(ui.group_name, "Respawn sniffer", true)
 ui.respawnmaster = menu.add_checkbox(ui.group_name, "Exit sniffer", true)
 ui.paradropmaster = menu.add_checkbox(ui.group_name, "ParaDrop sniffer", true)
 ui.dronedispatchmaster = menu.add_checkbox(ui.group_name, "Purchase Sniffer", true)
-
-
-
-
-
-
-
-
-
--- gui.SetValue("misc.log.console", true)
-
 local function findthisguy(thisguy, tab)
     if tab == nil then return end
     for i, guy in ipairs(tab) do
@@ -55,8 +21,7 @@ end
 local playerlist = {}
 local ingamestatus = false
 local cachelist = {}
--- local cachelistpurchaseid = {}
--- local cachemoneylist = {}
+
 local deadlist = {}
 local player_respawn_times = {}
 local tabletitemindex = {
@@ -144,7 +109,6 @@ local function main_exec()
                             string.gsub(': ' .. playername, '%s', '') .. addstr)
                         deadlist[playername] = nil
                     end
-                    -- deadlist = {}
                 end
                 local playerteamid = player:get_prop("m_nSurvivalTeam")
                 if localindex ~= playerIndex and ui.exitmaster:get() and needupdatecssplayer then
@@ -155,34 +119,8 @@ local function main_exec()
                         table.insert(playerlist, player:get_name())
                     end
                 end
-
-
-                -- if not player:IsAlive() and ingamestatus and respawnmaster:GetValue() then
-                --     deadlist[playername] = true
-                -- end
-                -- if player:GetWeaponID() == 72 and ingamestatus and purchasemaster:GetValue() then
-                --     local playerMoney = player:GetPropInt("m_iAccount")
-                --     local purchaseIndex = (player:GetPropEntity("m_hActiveWeapon")):GetPropInt("m_nLastPurchaseIndex")
-                --     moneylist[playerIndex] = playerMoney
-
-                --     if cachelistpurchaseid[playerIndex] == nil then
-                --         cachelistpurchaseid[playerIndex] = purchaseIndex
-                --     end
-                --     if cachemoneylist[playerIndex] == nil then
-                --         cachemoneylist[playerIndex] = playerMoney
-                --     end
-                --     if cachelistpurchaseid[playerIndex] ~= purchaseIndex then
-                --         if cachemoneylist[playerIndex] - playerMoney > 0 and purchaseIndex ~= -1 then
-                --             partyapisay(string.gsub(player:GetName(), '%s', '') ..
-                --                 "_purchased_" .. tabletitemindex[purchaseIndex])
-                --         end
-                --         cachelistpurchaseid[playerIndex] = purchaseIndex
-                --     end
-                -- end
             end
         end
-        -- if ingamestatus and purchasemaster:GetValue() then cachemoneylist = moneylist end
-
         if (#cachelist ~= #playerlist or #cachelist == 0) and ui.exitmaster:get() and #playerlist ~= 0 then
             if teammateisin and teammatenoshow then
                 teammatenoshow = false
@@ -193,9 +131,6 @@ local function main_exec()
                 partyapisay("Teammate_Exit" .. ":" .. string.gsub(teammatename, '%s', ''))
                 teammatenoshow = true
             end
-
-
-
             for _, enemy in ipairs(cachelist) do
                 if not findthisguy(enemy, playerlist) and enemy ~= teammatename then
                     if ingamestatus then
@@ -227,8 +162,6 @@ end
 local function on_event(e)
     local eventName = e.name
     if (eventName == "client_disconnect") or (eventName == "begin_new_match") then
-        -- cachelistpurchaseid = {}
-        -- cachemoneylist = {}
         cachelist = {}
         deadlist = {}
         lastcssplayernumber = 0
@@ -251,7 +184,7 @@ local function on_event(e)
     if eventName == "player_death" and ingamestatus then
         deadlist[(entity_list.get_player_from_userid(e.userid)):get_name()] = true
 
-        local teamid = (entity_list.get_player_from_userid(e.userid)):get_name():get_prop("m_nSurvivalTeam")
+        local teamid = (entity_list.get_player_from_userid(e.userid)):get_prop("m_nSurvivalTeam")
         if teamid == -1 or teamid == nil then return end
         local playername = (entity_list.get_player_from_userid(e.userid)):get_name()
         if player_respawn_times[playername] then
@@ -264,77 +197,75 @@ end
 callbacks.add(e_callbacks.EVENT, on_event)
 callbacks.add(e_callbacks.SETUP_COMMAND, main_exec)
 
--- local m_kg = gui.Button(main_box, "Check DZ Team", function()
---     if client.GetConVar("game_type") ~= "6" then
---         partyapisay("Not_DangerZone_Mode!")
---         return
---     end
---     local playerdata = {}
---     local abuseteam = {}
---     local nonsingleteamout = {}
---     local players = entities.FindByClass("CCSPlayer")
---     if players ~= nil then
---         for i, player in ipairs(players) do
---             local playerName = player:GetName()
---             local playerIndex = player:GetIndex()
---             if playerName ~= "GOTV" then
---                 local playerTeam = player:GetPropInt("m_nSurvivalTeam")
---                 local teamstr = "team" .. playerTeam
---                 if entities.GetPlayerResources():GetPropInt("m_bHasCommunicationAbuseMute", playerIndex) == 1 and teamstr ~= "team-1" then
---                     abuseteam[teamstr] = true
---                 end
---                 if playerdata[teamstr] == nil then
---                     playerdata[teamstr] = {}
---                 end
---                 table.insert(playerdata[teamstr],
---                     { playerIndex, playerName, playerTeam, player:IsAlive() })
---             end
---         end
+menu.add_button(ui.group_name, "Check DZ Team",function()
+    if cvars.game_type:get_int() ~= "6" then
+        partyapisay("Not_DangerZone_Mode!")
+        return
+    end
+    local playerdata = {}
+    local abuseteam = {}
+    local nonsingleteamout = {}
+    local players = entity_list.get_players(false)
+    if players ~= nil then
+        for _, player in ipairs(players) do
+            local playerName = player:get_name()
+            local playerIndex = player:get_index()
+            if playerName ~= "GOTV" then
+                local playerTeam = player:get_prop("m_nSurvivalTeam")
+                local teamstr = "team" .. playerTeam
+                if player_resource.get_prop("m_bHasCommunicationAbuseMute", playerIndex) == 1 and teamstr ~= "team-1" then
+                    abuseteam[teamstr] = true
+                end
+                if playerdata[teamstr] == nil then
+                    playerdata[teamstr] = {}
+                end
+                table.insert(playerdata[teamstr],
+                    { playerIndex, playerName, playerTeam, player:is_alive() })
+            end
+        end
 
---         partyapisay("--------------------------------------")
---         for i, player in ipairs(players) do
---             local teamstr = "team" .. player:GetPropInt("m_nSurvivalTeam")
---             local playerResources = entities.GetPlayerResources()
---             local communicationMute = playerResources:GetPropInt("m_bHasCommunicationAbuseMute", player:GetIndex())
---             local playerName = player:GetName()
---             if teamstr == "team-1" and playerName ~= "GOTV" then
---                 if communicationMute == 1 then
---                     partyapisay(string.gsub(playerName, '%s', '') .. "=Cheater_Solo")
---                 else
---                     partyapisay(string.gsub(playerName, '%s', '') .. "=Solo")
---                 end
---             else
---                 local playerTeamData = playerdata[teamstr]
---                 if playerTeamData ~= nil then
---                     if #playerTeamData ~= 1 then
---                         for j, data in ipairs(playerTeamData) do
---                             if data[1] ~= player:GetIndex() then
---                                 local teammateString = abuseteam[teamstr] and "CheaterTeammate:" or "Teammate:"
---                                 nonsingleteamout[teamstr] = teamstr ..
---                                     ":" ..
---                                     string.gsub(player:GetName(), '%s', '') ..
---                                     "_" .. teammateString .. string.gsub(data[2], '%s', '')
---                             end
---                         end
---                     else
---                         if communicationMute == 1 then
---                             nonsingleteamout[teamstr] = string.gsub(player:GetName(), '%s', '') ..
---                                 "=Might_Cheater_Solo"
---                         else
---                             nonsingleteamout[teamstr] = string.gsub(player:GetName(), '%s', '') .. "=Might_Solo"
---                         end
---                     end
---                 end
---             end
---         end
---         for i = 0, 12 do
---             local teamstr = "team" .. i
---             if nonsingleteamout[teamstr] then
---                 partyapisay(nonsingleteamout[teamstr])
---             end
---         end
---         partyapisay("Total:_" .. #players .. "_players")
---         partyapisay("-----------------END------------------")
---     end
--- end)
--- m_kg:SetWidth(268)
+        partyapisay("--------------------------------------")
+        for i, player in ipairs(players) do
+            local teamstr = "team" .. player:get_prop("m_nSurvivalTeam")
+            local communicationMute = player_resource.get_prop("m_bHasCommunicationAbuseMute", player:get_index())
+            local playerName = player:get_name()
+            if teamstr == "team-1" and playerName ~= "GOTV" then
+                if communicationMute == 1 then
+                    partyapisay(string.gsub(playerName, '%s', '') .. "=Cheater_Solo")
+                else
+                    partyapisay(string.gsub(playerName, '%s', '') .. "=Solo")
+                end
+            else
+                local playerTeamData = playerdata[teamstr]
+                if playerTeamData ~= nil then
+                    if #playerTeamData ~= 1 then
+                        for j, data in ipairs(playerTeamData) do
+                            if data[1] ~= player:get_index() then
+                                local teammateString = abuseteam[teamstr] and "CheaterTeammate:" or "Teammate:"
+                                nonsingleteamout[teamstr] = teamstr ..
+                                    ":" ..
+                                    string.gsub(player:get_name(), '%s', '') ..
+                                    "_" .. teammateString .. string.gsub(data[2], '%s', '')
+                            end
+                        end
+                    else
+                        if communicationMute == 1 then
+                            nonsingleteamout[teamstr] = string.gsub(player:get_name(), '%s', '') ..
+                                "=Might_Cheater_Solo"
+                        else
+                            nonsingleteamout[teamstr] = string.gsub(player:get_name(), '%s', '') .. "=Might_Solo"
+                        end
+                    end
+                end
+            end
+        end
+        for i = 0, 12 do
+            local teamstr = "team" .. i
+            if nonsingleteamout[teamstr] then
+                partyapisay(nonsingleteamout[teamstr])
+            end
+        end
+        partyapisay("Total:_" .. #players .. "_players")
+        partyapisay("-----------------END------------------")
+    end
+end)
